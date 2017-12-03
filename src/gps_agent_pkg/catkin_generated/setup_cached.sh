9fabb076699fd4c312e43cc1ad3ab8df1fd31c93
Jack Liu <jacknlliu@gmail.com>
2017-12-03 15:57:37 +0800

    init development commit

---
+++ src/gps_agent_pkg/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ros/data/gps-ur/src/gps_agent_pkg/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib/typelib:/opt/ros/indigo/lib:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/typelib:/opt/ros/indigo/lib/typelib:/opt/ros/indigo/lib/typelib"
+export LUA_CPATH=";;/opt/ros/indigo/lib/lua/5.1/?.so;/opt/ros/indigo/lib/lua/5.1/?.so;/opt/ros/indigo/lib/lua/5.1/?.so;/opt/ros/indigo/lib/lua/5.1/?.so"
+export LUA_PATH=";;/opt/ros/indigo/share/lua/5.1/?.lua;/opt/ros/indigo/share/lua/5.1/kdl_typekit/?.lua;/opt/ros/indigo/share/lua/5.1/?.lua;/opt/ros/indigo/share/lua/5.1/kdl_typekit/?.lua;/opt/ros/indigo/share/lua/5.1/?.lua;/opt/ros/indigo/share/lua/5.1/kdl_typekit/?.lua;/opt/ros/indigo/share/lua/5.1/?.lua;/opt/ros/indigo/share/lua/5.1/kdl_typekit/?.lua"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ros/data/gps-ur/src/gps_agent_pkg/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/data/gps-ur/src/gps_agent_pkg:/home/ros/data/gps-ur/src/gps_agent_pkg:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
+export RTT_COMPONENT_PATH="/home/ros/data/gps-ur/src/gps_agent_pkg/devel/lib/orocos:$RTT_COMPONENT_PATH"
